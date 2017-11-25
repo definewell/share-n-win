b36f95bb29e18868f6942807492c1c43661ac644
Kbhkn <hakan.kabasakal@bil.omu.edu.tr>
2017-11-26 00:05:55 +0300

    hakan menu

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DrawerNavigator } from 'react-navigation';
 import Movies from './RestExample'
 import OfferScreen from './screens/OfferScreen'
 import LoginScreen from './screens/LoginScreen'
 import Header from './screens/Header'
 
+const SimpleApp = DrawerNavigator({
+    Home: {
+        screen: Header,
+    }
+});
+
 export default class App extends React.Component {
 
   render() {
-    return (
-      <Header />
-    );
+      return <SimpleApp />;
+    // return (
+    //   <Header />
+    // );
   }
 }
 
--- screens/Header.js
+++ screens/Header.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View , Image} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableHighlight} from 'react-native';
 
 export default class Header extends React.Component {
 
-  render() {
-    return (
-        <View>
-            <Image
-            style={{width: 40, height: 40,  marginTop: 30,marginLeft:10, marginBottom:15}}
-            source={require('../img/menu.png')}
-            />
+    static navigationOptions = {
+        drawerLabel: 'Home'
+    };
 
-            <Image
-            source={require('../img/card.png')}
-            />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <TouchableHighlight onPress={() => this.props.navigation.navigate('DrawerOpen')}>
+                    <Image
+                        style={{width: 40, height: 40, marginTop: 30, marginLeft: 10, marginBottom: 15}}
+                        source={require('../img/menu.png')}
+                    />
+                </TouchableHighlight>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
